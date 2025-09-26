4529023b6cd2eda5a8912ba2be1c360986dd4edd
CENTIC <CENTIC@10.6.65.83>
2025-09-26 12:04:47 -0500

    rama3.1

--- src/Booking/Train.java
+++ src/Booking/Train.java
@@ -9,5 +9,44 @@ package Booking;
  * @author CENTIC
  */
 public class Train {
+    private String seatType;
+    private int wagonNumber;
     
+    public Train(){
+        
+    }
+    public Train (int id, String origin, String destinaton, double price, String seatType, int wagonNumber){
+        
+        this.setSeatType(seatType);
+        this.setWagonNumber(wagonNumber);
+    }
+
+    public String getSeatType() { //0
+        return seatType;
+    }
+
+    public final void setSeatType(String seatType) {
+        this.seatType = seatType;
+    }
+
+    public int getWagonNumber() {
+        return wagonNumber;
+    }
+
+    public final void setWagonNumber(int wagonNumber) {
+        this.wagonNumber = wagonNumber;
+    }
+    
+    public String getServiceInformation(){
+        return null;
+    }
+    
+    public String getLuggageInformation(){
+        return null;
+    }
+    
+    @Override
+    public String toString(){
+        return null;
+    }
 }
