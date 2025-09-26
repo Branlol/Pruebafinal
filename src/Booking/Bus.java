04cb677e2c643f3df2cf170fc8912469d1cdb766
CENTIC <CENTIC@10.6.65.41>
2025-09-26 12:22:46 -0500

    Revert "Rama1.3"
    This reverts commit bd226cf8127a028f8323b2a05468edcf52b467b4.

--- src/Booking/Bus.java
+++ src/Booking/Bus.java
@@ -1,48 +1,13 @@
-
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
 package Booking;
 
-
-abstract class Bus extends TransportService{
+/**
+ *
+ * @author CENTIC
+ */
+public class Bus {
     
-    private String busCompany;
-    private String boardingPoint;
-
-    public Bus() {
-    }
-
-    public Bus(String busCompany, String boardingPoint) {
-        this.setBusCompany(busCompany);
-        this.setBoardingPoint(boardingPoint);
-    }
-    
-    public void setBusCompany(String busCompany) {
-        this.busCompany = busCompany;
-    }
-    public void setBoardingPoint(String boardingPoint) {
-        this.boardingPoint = boardingPoint;
-    }
-
-    public String getBusCompany() {
-        return busCompany;
-    }
-   
-    public String getBoardingPoint() {
-        return this.boardingPoint;
-    }
-    
-    public String getServiceInformation(){
-        String str = "\n" + this.getId() + " is active" ;
-        return str;
-    }
-    @Override
-    public String getLuggageInformation(){
-        String str = "\ncollect luggage from the bus" + this.getId();
-        return str;
-    }
-    @Override
-    public String toString() {
-        String str = super.toString() + "\nCompany: " + this.getBusCompany() + "\nBoarding point: " + this.boardingPoint;
-        return str;
-    }
-
 }
